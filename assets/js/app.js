9ac910b27adf0200f876bc3faeffac827918673e
ProgBot12 <emir.compwiz@gmail.com>
2021-01-19 12:14:39 -0500

    Added font-awesome icons

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,7 +2,7 @@ const carouselSlide = document.querySelector(".carousel-slide");
 const carouselImages = document.querySelectorAll(".carousel-slide img");
 
 //Buttons
-const prevButton = document.querySelector("#prevBtn");
+const prevButton = document.querySelector("#previousBtn");
 const nextButton = document.querySelector("#nextBtn");
 
 //Counter
@@ -13,13 +13,30 @@ carouselSlide.style.transform = "translateX(" + -size * counter + "px)";
 
 //Button listeners
 nextButton.addEventListener("click", () => {
+  if (counter >= carouselImages.length - 1) return;
+
   carouselSlide.style.transition = "transform 0.4s ease-in-out";
   counter++;
   carouselSlide.style.transform = "translateX(" + -size * counter + "px)";
 });
 
 prevButton.addEventListener("click", () => {
+  if (counter <= 0) return;
+
   carouselSlide.style.transition = "transform 0.4s ease-in-out";
   counter--;
   carouselSlide.style.transform = "translateX(" + -size * counter + "px)";
+});
+
+carouselSlide.addEventListener("transitionend", () => {
+  if (carouselImages[counter].id == "lastClone") {
+    carouselSlide.style.transition = "none";
+    counter = carouselImages.length - 2;
+    carouselSlide.style.transform = "translateX(" + -size * counter + "px)";
+  }
+  if (carouselImages[counter].id == "firstClone") {
+    carouselSlide.style.transition = "none";
+    counter = carouselImages.length - counter;
+    carouselSlide.style.transform = "translateX(" + -size * counter + "px)";
+  }
 });
